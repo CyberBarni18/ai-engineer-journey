246d46aa7b1389a17d833676fbc2ec358f984aaa
CyberBarni18 <barnivet78@gmail.com>
2026-03-11 21:24:53 +0100

    Day1 Python Basics

---
+++ day1_basics.py
@@ -0,0 +1,36 @@
+print("Hello AI Journey")
+
+# változók
+name = "AI Engineer Journey"
+hours_per_day = 3
+goal_year = 2027
+
+print(name)
+print(hours_per_day)
+print(goal_year)
+
+# számolás
+daily_hours = 3
+days_per_year = 365
+
+total_hours = daily_hours * days_per_year
+
+print("Study hours in a year:", total_hours)
+
+# lista
+species = ["fox", "deer", "hawk", "rabbit"]
+
+print(species)
+print("First species:", species[0])
+
+# for loop
+for animal in species:
+    print("Observed:", animal)
+
+# mini feladat
+species = ["fox", "deer", "hawk", "rabbit", "fox"]
+
+unique_species = set(species)
+
+print("Total observations:", len(species))
+print("Unique species:", len(unique_species))
